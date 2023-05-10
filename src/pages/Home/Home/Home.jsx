f51f213c43414e2d97a2ca66192910f5f53d11e9
getahsaan <getahsaan@gamil.com>
2023-05-10 18:19:59 +0600

    project init, navbar + footer added

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-center text-4xl text-red-500">This is Home</h2>
+    </div>
+  );
+};
+
+export default Home;
