de8716e17d05b0e0479002295913ad27cc700182
getahsaan <getahsaan@gamil.com>
2023-05-10 19:15:37 +0600

    banner section added

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
     <div>
-      <h2 className="text-center text-4xl text-red-500">This is Home</h2>
+      <Banner></Banner>
     </div>
   );
 };
