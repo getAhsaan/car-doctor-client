317414737b3c3ca450a6c452dac023823123bc39
getahsaan <getahsaan@gamil.com>
2023-05-11 11:53:30 +0600

    context api added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -5,6 +5,9 @@ import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:gap-32 lg:flex-row">
@@ -17,37 +20,45 @@ const Login = () => {
         <div className="card w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
             <h2 className="text-4xl text-center font-bold">Login</h2>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-              />
-              <label className="label">
-                <a
-                  href="#"
-                  className="label-text-alt link link-hover"
-                >
-                  Forgot password?
-                </a>
-              </label>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign In</button>
-            </div>
+            <form onSubmit={handleLogin}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+                <label className="label">
+                  <a
+                    href="#"
+                    className="label-text-alt link link-hover"
+                  >
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign In"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
             <p className="text-center">Or Sign In with</p>
 
             <div className="flex mx-auto gap-4">
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import loginPic from "../../assets/images/login/login.svg";
-import { FcGoogle } from "react-icons/fc";
-import { FaGithub } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+  const handleRegister = (event) => {
+    event.preventDefault();
+  };
   return (
     <div className="hero min-h-screen bg-base-200">
       <div className="hero-content flex-col lg:gap-32 lg:flex-row">
@@ -17,39 +18,48 @@ const Register = () => {
         <div className="card w-full max-w-sm shadow-2xl bg-base-100">
           <div className="card-body">
             <h2 className="text-4xl text-center font-bold">Register</h2>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder="name"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Password</span>
-              </label>
-              <input
-                type="password"
-                placeholder="password"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">Sign Up</button>
-            </div>
+            <form onSubmit={handleRegister}>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="name"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                />
+              </div>
+              <div className="form-control mt-6">
+                <input
+                  type="submit"
+                  value="Sign Up"
+                  className="btn btn-primary"
+                />
+              </div>
+            </form>
             <p className="text-center">
               Have an account? Please{" "}
               <Link
